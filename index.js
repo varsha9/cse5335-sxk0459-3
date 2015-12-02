6460d23f8f40f9f1883f68fcdef300e11313fcfc
Sri Varsha <CaptainVarsha@Sris-MacBook-Pro.local>
2015-12-02 14:24:23 -0600

    code pushed

--- index.js
+++ index.js
@@ -1,20 +1,70 @@
 var express = require('express');
 var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
+var assert = require('assert');
+app.use(express.static(__dirname  + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+
+
+
+
+app.listen(process.env.PORT || 5000);
+
+app.get('/', function(request, response)
+{
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/quotes/:id', function(req, res) {
+
+  console.log(req.params.id);
+
+  var a =   req.params.id;
+
+  //jjjj
+
+
+  var connect = require('connect'),
+      mongo = require('mongodb');
+// var prompt = require('prompt');
+  var assert = require('assert');
+  mongo.connect('mongodb://heroku_9695fwwt:dqq4976el73qnuqu2ivgho4qeo@ds059284.mongolab.com:59284/heroku_9695fwwt', {}, function(error, db) {
+
+    if (error) {
+      return console.log("Not connected");
+    }
+
+    console.log("Connected");
+
+
+
+    var findpeople = function (db, callback) {
+      var cursor = db.collection('Pro').find({"_id": +a });
+
+      cursor.each(function (err, doc) {
+        assert.equal(err, null);
+        if (doc != null) {
+          console.dir(doc);
+          res.send(doc);
+
+        } else {
+          callback();
+        }
+      });
+    }
+    mongo.connect('mongodb://heroku_d03kn7hw:4pd26pbsinojodrga1hkp3sjv5@ds045604.mongolab.com:45604/heroku_d03kn7hw', function(err, db) {
+      assert.equal(null, err);
+      findpeople(db, function() {
+        db.close();
+      });
+    });
+  });
+
+
+  //jjj
+
+
 });
 
 
